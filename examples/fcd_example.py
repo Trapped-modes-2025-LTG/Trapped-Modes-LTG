b1c5f1145eb5f37d609f6dc8a1c0cb9aaf700fcd
Juan Giordano <juan.giordano02@gmail.com>
2025-03-23 19:52:42 -0300

    Fix imports

--- examples/fcd_example.py
+++ examples/fcd_example.py
@@ -3,8 +3,9 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pyfcd.fcd import compute_height_map
 
-reference_path = os.path.join('examples/pictures/', 'reference_2.png')
-displaced_path = os.path.join('examples/pictures/', '202406_1457001661.bmp')
+base_dir = os.path.dirname(__file__)
+reference_path = os.path.join(base_dir, 'Pictures', 'reference_2.png')
+displaced_path = os.path.join(base_dir, 'Pictures', '202406_1457001661.bmp')
 height_map = compute_height_map(reference_path, displaced_path, square_size=0.0022, height=0.0323625)
 
 height_map.show()
