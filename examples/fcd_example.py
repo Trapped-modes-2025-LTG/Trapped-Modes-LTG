cc806e3bd042e3bc5810a23a0c351849411ac66a
Juan Giordano <juan.giordano02@gmail.com>
2025-03-23 19:47:57 -0300

    Update fcd_example.py

--- examples/fcd_example.py
+++ examples/fcd_example.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pyfcd.fcd import compute_height_map
 
 reference_path = os.path.join('examples/pictures/', 'reference_2.png')
