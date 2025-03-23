6c69117af1fe61c89b4e27205c0f2aa1cc2c3e45
Juan Giordano <juan.giordano02@gmail.com>
2025-03-23 14:27:14 -0300

    Update fcd_example.py

--- examples/fcd_example.py
+++ examples/fcd_example.py
@@ -1,9 +1,8 @@
+import os
 route = '/home/juan/fcd-analysis/Codigo nuevo/prueba1'
-reference_path = '/home/juan/fcd-analysis/examples/static_images/reference_2.png'
-#reference_path = f"{route}/prueba1_20250317_122608_C1S0001000001.tif"
-displaced_path = f"{route}/prueba1_20250317_122608_C1S0001000158.tif"
-displaced_path = '/home/juan/fcd-analysis/examples/propagating_waves/202406_1457001661.bmp'
-# Crear el mapa de altura
+
+reference_path = os.path.join('examples/pictures/', 'reference_2.png')
+displaced_path = os.path.join('examples/pictures/', '202406_1457001661.bmp')
 height_map = compute_height_map(reference_path, displaced_path, square_size=0.0022, height=0.0323625)
 
 height_map.show()
