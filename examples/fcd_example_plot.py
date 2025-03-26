3dced2198d2651cf3af5c1014287f44eec46be18
Juan Giordano <juan.giordano02@gmail.com>
2025-03-26 10:18:29 -0300

    Update fcd_example_plot.py

--- examples/fcd_example_plot.py
+++ examples/fcd_example_plot.py
@@ -10,13 +10,11 @@ reference_path = os.path.join(base_dir, 'Pictures', 'reference_2.png')
 displaced_path = os.path.join(base_dir, 'Pictures', '202406_1457001661.bmp')
 values = compute_height_map(reference_path, displaced_path, square_size=0.0022, height=0.0323625)       #tuple = (height_map, phases, calibration_factor)
 
-
 x = np.linspace(0,values[0].shape[1],values[0].shape[1]) * values[2]
 y = np.linspace(0,values[0].shape[0],values[0].shape[0]) * values[2]
-
 x_mesh, y_mesh = np.meshgrid(x,y)
 
-fig, axis = plt.subplots()
+fig, axis = plt.subplots()        #Heightmap
 im = axis.contourf(x_mesh * 1e3, y_mesh * 1e3, values[0] * 1e3, 100)    
 cbar = fig.colorbar(im, ax=axis)
 cbar.set_label('Altura [mm]', rotation=270, labelpad=15)
@@ -24,8 +22,7 @@ axis.set_xlabel('Posición x [mm]')
 axis.set_ylabel('Posición y [mm]')
 axis.set_aspect("equal")    
 
-
-fig, axs = plt.subplots(1, 2)
+fig, axs = plt.subplots(1, 2)    #phases
 for i, angles in enumerate(values[1]):
     im = axs[i].contourf(x_mesh*1e3, y_mesh*1e3, values[1][i], 100)
     for c in im.collections: # Esto soluciona el problema de aliasing al guardar como .pdf.
