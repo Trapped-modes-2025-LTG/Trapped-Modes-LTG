3affe21a67049d8e50f52730cdfa088412e41669
Juan Giordano <juan.giordano02@gmail.com>
2025-03-31 13:51:55 -0300

    Add files via upload

---
+++ pyfcd/fcd_copy.py
@@ -0,0 +1,115 @@
+import numpy as np
+from scipy.fft import fft2, ifft2
+from skimage.restoration import unwrap_phase
+import pyfcd.fourier_space as fs
+from pyfcd.carriers import Carrier
+
+
+
+def compute_carriers(reference, calibration_factor, square_size):
+    """
+    Compute the carriers for the reference image.
+
+    Parameters:
+        reference_path (str): Path to the reference image.
+        calibration_factor (float): Calibration factor.
+        square_size (float): Size of the square pattern in meters.
+
+    Returns:
+        tuple: (reference image, carriers list, detected peaks)
+    """
+    peaks = fs.find_peaks(reference)
+    calibration_factor = compute_calibration_factor(peaks, square_size, reference)
+    peak_radius = np.linalg.norm(peaks[0] - peaks[1]) / 2
+    carriers = [Carrier(reference, calibration_factor, peak, peak_radius) for peak in peaks]
+    return reference, carriers, peaks, calibration_factor
+
+def compute_calibration_factor(peaks, square_size, reference):
+    """
+    Compute the calibration factor using the detected peaks.
+
+    Parameters:
+        peaks (array): Detected peak positions.
+        square_size (float): Size of the square pattern in meters.
+        reference (array): Reference image.
+
+    Returns:
+        float: Computed calibration factor.
+    """
+    pixel_frequencies = fs.pixel_to_wavenumber(reference.shape, peaks)
+    pixel_wavelength = 2 * np.pi / np.mean(np.abs(pixel_frequencies))
+    physical_wavelength = 2 * square_size
+    return physical_wavelength / pixel_wavelength
+
+def compute_phases(displaced_fft, carriers, unwrap=True):
+    """
+    Compute the phase maps from the displaced image.
+
+    Parameters:
+        displaced_fft (array): Fourier transform of the displaced image.
+        carriers (list): List of Carrier objects.
+        unwrap (bool): Whether to apply phase unwrapping.
+
+    Returns:
+        array: Phase maps for each carrier.
+    """
+    phases = np.zeros((2, *displaced_fft.shape))
+    for i, carrier in enumerate(carriers):
+        phase_angles = -np.angle(ifft2(displaced_fft * carrier.mask) * carrier.ccsgn)
+        phases[i] = unwrap_phase(phase_angles) if unwrap else phase_angles
+    return phases
+
+def compute_displacement_field(phases, carriers):
+    """
+    Compute the displacement field (u, v) from the phase maps.
+
+    Parameters:
+        phases (array): Phase maps.
+        carriers (list): List of Carrier objects.
+
+    Returns:
+        array: Displacement field (u, v).
+    """
+    det_a = carriers[0].frequencies[1] * carriers[1].frequencies[0] - \
+            carriers[0].frequencies[0] * carriers[1].frequencies[1]
+    u = (carriers[1].frequencies[0] * phases[0] - carriers[0].frequencies[0] * phases[1]) / det_a
+    v = (carriers[0].frequencies[1] * phases[1] - carriers[1].frequencies[1] * phases[0]) / det_a
+    return np.array([u, v])
+    
+#height_from_layers()
+def height_from_layers(layers):  # TODO: No sé si hay vidrio por ejemplo entre el agua y la cámara cómo cambia esto.
+    fluid = layers[-2]
+    before_camera = layers[-1]
+    alpha = 1 - before_camera.refractive_index / fluid.refractive_index
+
+    height = 0
+    for layer in layers[:-1]:
+        height += layer.effective_height(fluid.refractive_index)
+
+    return alpha * height
+
+
+def compute_height_map(reference, displaced, square_size,layers = None, height=None, unwrap=True):
+    
+    reference, carriers, peaks, calibration_factor = compute_carriers(reference, None, square_size)
+    #displaced = load_image(displaced_path)
+    displaced_fft = fft2(displaced)
+    
+    phases = compute_phases(displaced_fft, carriers, unwrap)
+    displacement_field = compute_displacement_field(phases, carriers)
+    
+    if height is not None:
+        if layers is None:
+            height = height  # TODO: le estoy mandando ya la altura efectiva.
+        else:
+            raise Warning("Provide either height or layers, not both.")
+    else:
+        if layers is None:
+            height = 1
+        else:
+            height = height_from_layers(layers)
+    
+    
+    height_gradient = -displacement_field / height    #TODO: height ya es el coeficiente de proporción.
+    height_map = fs.integrate_in_fourier(*height_gradient, calibration_factor)
+    return height_map, phases, calibration_factor
