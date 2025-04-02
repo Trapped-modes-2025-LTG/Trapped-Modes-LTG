62ce6e4c8c6a06a4880a088ae76df4f7ea61ada7
Juan Giordano <juan.giordano02@gmail.com>
2025-04-02 08:42:53 -0300

    Update val_sin.py

--- pyval/val_sin.py
+++ pyval/val_sin.py
@@ -12,7 +12,7 @@ all_data = []
 vmin, vmax = np.inf, -np.inf
 
 for  altura in alturas: 
-    X,Y, h,Ii, values,I0 = val(Asin,A = altura, centrado_si = True, k = 0)
+    X,Y, h,Ii, values,I0 = val(Asin, k = 0,A = altura, centrado_si = True)
     data = values - h.T/np.max(h.T)
     all_data.append(data)
     vmin = min(vmin, np.min(data))
@@ -22,11 +22,10 @@ N = 500
 
 fila_corte = N // 2  # Índice de la fila central
 
-cmap = cm.gnuplot  # Colormap 'magma'
+cmap = cm.gnuplot 
 norm = mcolors.Normalize(vmin=np.min(alturas), vmax=np.max(alturas))  # Normalización
 
 fig, ax = plt.subplots()
-#rec = []
 for altura, data in zip(alturas, all_data):
     x_vals = np.linspace(0, np.max(X), N)  # Eje X
     y_vals = data[fila_corte, :]  # Corte en la fila central
@@ -34,7 +33,6 @@ for altura, data in zip(alturas, all_data):
 
     ax.plot(x_vals, y_vals, color=color, label=f"H = {altura:.2f}")
 
-# Crear la barra de color
 sm = cm.ScalarMappable(cmap=cmap, norm=norm)
 cbar = fig.colorbar(sm, ax=ax)
 cbar.set_label("Altura")
@@ -44,8 +42,3 @@ ax.set_ylabel("Intensidad")
 ax.set_title("Cortes en la fila central para distintas alturas")
 ax.grid(linestyle = '--', alpha = 0.5)
 ax.plot()
-# plt.imshow(u[..., 1], cmap="magma", origin="lower", extent=[0, Lx, 0, Ly])
-# plt.colorbar(label=r'$u_y$')
-# plt.title(r'Componente $u_y$ del desplazamiento')
-
-# plt.show()
