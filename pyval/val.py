8d21e5cf47723c401bebaab096d0cce595c2d3e8
Juan Giordano <juan.giordano02@gmail.com>
2025-04-07 15:38:07 -0300

    Update val.py

--- pyval/val.py
+++ pyval/val.py
@@ -4,21 +4,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 from pyfcd.fcd import compute_height_map
+from pyfcd.fourier_space import wavenumber_meshgrid
 
 def h_grad(h,x,y,k=0):
-    X, Y = np.meshgrid(x, y, indexing='ij')
-    N = len(X)   #asumo cuadrada
+    X, Y = np.meshgrid(x, y, indexing='ij')    
     if k == 0:
+        kx, ky = wavenumber_meshgrid(h.shape)
         h_hat = np.fft.fft2(h)
-        Lx = np.max(X)
-        Ly = np.max(Y)
-        kx = (2 * np.pi / Lx) * np.fft.fftfreq(N, d=Lx/N)
-        ky = (2 * np.pi / Ly) * np.fft.fftfreq(N, d=Ly/N)
-        KX, KY = np.meshgrid(kx, ky, indexing='ij')
-
-        h_x_hat = 1j * KX * h_hat  
-        h_y_hat = 1j * KY * h_hat  
-
+        h_x_hat = 1j * kx * h_hat
+        h_y_hat = 1j * ky * h_hat
         h_x = np.fft.ifft2(h_x_hat).real
         h_y = np.fft.ifft2(h_y_hat).real
         grad = np.stack((h_x, h_y), axis=-1)
@@ -29,7 +23,7 @@ def h_grad(h,x,y,k=0):
         raise ValueError('Parámetro k entre 0 (pseudoespectral) y 1 (diferencias finitas)')
     return grad
 
-def val(func,k, N = 1024, Lx = np.pi, Ly = np.pi, H = 1, square_size = 1, kx = 20, ky = 20, centrado_si = False, *args, **kwargs):
+def val(k,func = None,h = None, N = 1024,  H = 1, square_size = 1, kx = 20, ky = 20, centrado_si = False, *args, **kwargs):
     '''
     Parámetros necesarios:
         'func': función usada. Necesariamente tiene que ser función de X e Y, con estructura 'func(X,Y,*kwargs*)'
@@ -42,27 +36,35 @@ def val(func,k, N = 1024, Lx = np.pi, Ly = np.pi, H = 1, square_size = 1, kx = 2
         'kx = ky = 20': frecuencia del patrón I_0
         'centrado_si = False': Centrar y normalizar los valores devueltos por la FCD
     '''
-    x = np.linspace(0, Lx, N)
-    y = np.linspace(0, Ly, N)
+    x = np.linspace(0, N, N, endpoint = False)
+    y = np.linspace(0, N, N, endpoint = False)
     X, Y = np.meshgrid(x, y, indexing='ij')
-    h = func(X,Y,*args, **kwargs)
+    if func is not None:
+        if h is None:
+            h = func(X,Y,*args, **kwargs)
+        else:
+            raise Warning("Provide either h or func, not both.")
+    else: 
+        if h is None:
+            raise Warning("Provide either h or func, not empty.")
+        else: 
+            h = h
     u = -H*h_grad(h,x,y,k=k)
     I0 = np.sin(kx * X) * np.sin(ky * Y)
     r = np.stack((X, Y), axis=-1)
     r_prim = (r - u)  
+    r_prim[..., 0] = np.clip(r_prim[..., 0], x.min(), x.max())
+    r_prim[..., 1] = np.clip(r_prim[..., 1], y.min(), y.max())
 
     interp_I0 = RegularGridInterpolator((x, y), I0, bounds_error=False, fill_value=0)
     I = interp_I0(r_prim.reshape(-1, 2)).reshape(N, N) 
-    Ii = I.T
-    Ii = np.flip(Ii, axis=(0, 1))  # Invierte en ambas direcciones
-    values = compute_height_map(I0, Ii, square_size=square_size, height=H)[0]     #tuple = (height_map, phases, calibration_factor)
+    values = compute_height_map(I0, I, square_size=square_size, height=H)[0]     #tuple = (height_map, phases, calibration_factor)
     
     if centrado_si == True:
         values = centrado(values)
     else:
         pass
-    
-    return X,Y,h, Ii, values, I0
+    return X,Y,h, I, values, I0
 
 def centrado(v):
     meanh = (np.max(v) + np.min(v))*0.5
