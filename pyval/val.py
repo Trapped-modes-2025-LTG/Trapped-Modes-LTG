8629e55c92b19424d5490f5ddf7a266431e03761
Juan Giordano <juan.giordano02@gmail.com>
2025-04-08 14:01:53 -0300

    Direct interpolate

--- pyval/val.py
+++ pyval/val.py
@@ -1,5 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
-from scipy.interpolate import RegularGridInterpolator
+#from scipy.interpolate import RegularGridInterpolator
 from pyfcd.fcd import compute_height_map
 from pyfcd.fourier_space import wavenumber_meshgrid
 
@@ -21,17 +24,17 @@ def h_grad(h,x,y,k=0):
         raise ValueError('Parámetro k entre 0 (pseudoespectral) y 1 (diferencias finitas)')
     return grad
 
-def val(k,func = None,h = None, N = 1024,  H = 1, square_size = 1, kx = 20, ky = 20, centrado_si = False, *args, **kwargs):
+def val(k,func = None,h = None, N = 1024,  H = 1, square_size = 1, nx = 20, ny = 20, centrado_si = False, *args, **kwargs):
     '''
     Parámetros necesarios:
-        'k': integrador a usar. k = 0 pseudoespectral, k = 1 diferencias finitas
         'func': función usada. Necesariamente tiene que ser función de X e Y, con estructura 'func(X,Y,*kwargs*)'
-        'h': campo de alturas directamente de tenerlo      
+        'k': integrador a usar. k = 0 pseudoespectral, k = 1 diferencias finitas
         '**kwargs': parámetros necesarios de 'func', como amplitud, fase, frecuencia, o lo necesario.
     Parámetros opcionales (llamarlos con otro valor si se quiere modificarlos):
-        'N = 1024': Grillado X e Y
+        'N = 500': Grillado X e Y
+        'Lx = Ly = np.pi': puntos finales de ambas coordenadas
         'H = 1': altura efectiva del agua. 
-        'kx = ky = 20': frecuencia del patrón I_0
+        'nx = ny = 20': número de cíclos del patrón de fondo
         'centrado_si = False': Centrar y normalizar los valores devueltos por la FCD
     '''
     x = np.linspace(0, N, N, endpoint = False)
@@ -48,14 +51,18 @@ def val(k,func = None,h = None, N = 1024,  H = 1, square_size = 1, kx = 20, ky =
         else: 
             h = h
     u = -H*h_grad(h,x,y,k=k)
-    I0 = 0.5 + (np.cos(x *kx) + np.cos(y * ky)) / 4.0
+    
+    kx = 2 * np.pi * nx / N
+    ky = 2 * np.pi * ny / N
+    I0 = 0.5 + (np.cos(X *kx) + np.cos(Y * ky)) / 4.0
     r = np.stack((X, Y), axis=-1)
     r_prim = (r - u)  
     r_prim[..., 0] = np.clip(r_prim[..., 0], x.min(), x.max())
     r_prim[..., 1] = np.clip(r_prim[..., 1], y.min(), y.max())
+
     # interp_I0 = RegularGridInterpolator((x, y), I0, bounds_error=False, fill_value=0)
     # I = interp_I0(r_prim.reshape(-1, 2)).reshape(N, N) 
-    I = 0.5 + (np.cos(r_prim[..., 0] * kx) + np.cos(r_prim[..., 1] * ky)) / 4.0     #interpolo directamente evaluando en el patron 
+    I = 0.5 + (np.cos(r_prim[..., 0] * kx) + np.cos(r_prim[..., 1] * ky)) / 4.0     #interpolo directamente evaluando en el patron
     values = compute_height_map(I0, I, square_size=square_size, height=H)[0]     #tuple = (height_map, phases, calibration_factor)
     
     if centrado_si == True:
