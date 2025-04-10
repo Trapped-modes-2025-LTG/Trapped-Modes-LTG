c9cb30bf9c2c6afa6401e9d1533c776c29f28faa
Juan Giordano <juan.giordano02@gmail.com>
2025-04-10 17:49:30 -0300

    new square_size for calibration_factor = 1

--- pyval/val.py
+++ pyval/val.py
@@ -24,7 +24,7 @@ def h_grad(h,x,y,k=0):
         raise ValueError('Parámetro k entre 0 (pseudoespectral) y 1 (diferencias finitas)')
     return grad
 
-def val(k,func = None,h = None, N = 1024,  H = 1, square_size = 1, nx = 20, ny = 20, centrado_si = False, *args, **kwargs):
+def val(k,func = None,h = None, N = 1024,  H = 1, n = 20, centrado_si = False, *args, **kwargs):
     '''
     Parámetros necesarios:
         'func': función usada. Necesariamente tiene que ser función de X e Y, con estructura 'func(X,Y,*kwargs*)'
@@ -37,6 +37,7 @@ def val(k,func = None,h = None, N = 1024,  H = 1, square_size = 1, nx = 20, ny =
         'nx = ny = 20': número de cíclos del patrón de fondo
         'centrado_si = False': Centrar y normalizar los valores devueltos por la FCD
     '''
+    square_size = N/(2*n)       #para querer que el factor de calibración sea 1, este es el tamaño del cuadrado
     x = np.linspace(0, N, N, endpoint = False)
     y = np.linspace(0, N, N, endpoint = False)
     X, Y = np.meshgrid(x, y, indexing='ij')
@@ -52,17 +53,17 @@ def val(k,func = None,h = None, N = 1024,  H = 1, square_size = 1, nx = 20, ny =
             h = h
     u = -H*h_grad(h,x,y,k=k)
     
-    kx = 2 * np.pi * nx / N
-    ky = 2 * np.pi * ny / N
-    I0 = 0.5 + (np.cos(X *kx) + np.cos(Y * ky)) / 4.0
+    kx = 2 * np.pi * n / N
+    ky = 2 * np.pi * n / N
+    I0 = 0.5 + (np.cos(X *kx) * np.cos(Y * ky)) / 2
     r = np.stack((X, Y), axis=-1)
     r_prim = (r - u)  
     r_prim[..., 0] = np.clip(r_prim[..., 0], x.min(), x.max())
     r_prim[..., 1] = np.clip(r_prim[..., 1], y.min(), y.max())
 
-    # interp_I0 = RegularGridInterpolator((x, y), I0, bounds_error=False, fill_value=0)
-    # I = interp_I0(r_prim.reshape(-1, 2)).reshape(N, N) 
-    I = 0.5 + (np.cos(r_prim[..., 0] * kx) + np.cos(r_prim[..., 1] * ky)) / 4.0     #interpolo directamente evaluando en el patron
+    interp_I0 = RegularGridInterpolator((x, y), I0, bounds_error=False, fill_value=0)
+    I = interp_I0(r_prim.reshape(-1, 2)).reshape(N, N) 
+    #I = 0.5 + (np.cos(r_prim[..., 0] * kx) + np.cos(r_prim[..., 1] * ky)) / 4.0     #interpolo directamente evaluando en el patron
     values = compute_height_map(I0, I, square_size=square_size, height=H)    #tuple = (height_map, phases, calibration_factor)
     calibration_factor = values[2]
     if centrado_si == True:
