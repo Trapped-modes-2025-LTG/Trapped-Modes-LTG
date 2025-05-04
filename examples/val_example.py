f25aaa7d4f91399beb313945929fcd8a74fdafa9
Juan Giordano <juan.giordano02@gmail.com>
2025-05-04 11:04:48 -0300

    Documentate val_example.py

--- examples/val_example.py
+++ examples/val_example.py
@@ -1,3 +1,16 @@
+'''
+Example script for make a synthetic validation using "/pyval/val.py". Check the parameters 
+of "val()" in its script.
+
+Here, an armonic wave modulated by a central-square step was validated. Other several 
+function would be checked. Its important for the pseudospectral method that they has a mean
+equal to zero. 
+
+The displaced map, the re-build surface height map and the difference between the last one
+and the original height map were shown in a plot. The maximum percentage difference between 
+the two height maps is plotted too.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -11,25 +24,18 @@ def step(X,a= 0.02,w = 400):
 def gauss_sin(X,Y, A = 100, w = 0.05):
     return step(X)*step(Y)*A*np.sin(w*(X+Y))
     
-# elegir la función deseada, y si se quiere cambiar los parámetros:
-    # el integrador (k = 0 o k = 1)
-    # la frecuencia de puntos N
-    # la altura efectiva H
-    # el espaciado de los puntos del patrón n
-# en este caso, está todo definido para que el factor de calibración devuelto sea 1. Simplemente para la simulación.
-    
 X,Y, h,Ii, values,I0, calibration_factor = val(0, func = gauss_sin, centrado_si = False)
 
 fig, ax = plt.subplots(1,3, figsize = (10,4))
 
 im0 = ax[0].imshow(Ii, origin='lower')
 cbar = fig.colorbar(im0, ax=ax[0], orientation='horizontal')
-ax[0].set_title('Patrón deformado')
+ax[0].set_title('Displaced pattern')
 
 im1 = ax[1].imshow(values, origin='lower')
 cbar = fig.colorbar(im1, ax=ax[1], orientation='horizontal')
-ax[1].set_title('Mapa devuelto por la FCD')
+ax[1].set_title('Height map returned by FCD')
 
 im2 = ax[2].imshow(values-h, origin='lower')
 cbar = fig.colorbar(im2, ax=ax[2], orientation='horizontal')
-ax[2].set_title(r'Resta de alturas: <' + f'{np.max(np.abs(values-h))*100/(np.max(np.abs(values))):.2f}%')
+ax[2].set_title(r'Height differences: <' + f'{np.max(np.abs(values-h))*100/(np.max(np.abs(values))):.2f}%')
