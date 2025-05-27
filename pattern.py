93fd78ae6ab606b70b62ee8ee15c35aa18ffbb79
JBGiordano <juan.giordano02@gmail.com>
2025-05-27 15:38:50 -0300

    Mask as a statical method and new pattern.py

--- pattern.py
+++ pattern.py
@@ -1,40 +1,36 @@
-'''
-Script for generate the patterns which we used for measure. 
-
-In our setup, we place the printed pattern on top of an LED backlight, and both are positioned beneath a water tank. A camera is mounted vertically 
-above the setup to capture images of the displaced pattern. Using the "pyfcd" library, we obtain the height map of the surface.
-
-Checkerboard patterns are of particular interest. We generate them with 16-bit resolution to match the 10-bit acquisition capability of our camera.
-To generate these patterns, we need to know the printer's resolution, the size of the LED backlight, and the desired size of each square in the checkerboard.
-
-Parameters:
-    N: pixels of the pattern. Deppend of the printer's resolution
-    l: lenght of the LED backlight in cm. 
-    mm: lenght of each square in mm
-'''
-
 import numpy as np
 import tifffile
-cm_i = 2.54                     # cm per inch
 
-N = 4096                         
-l = 44                          
-mm = 2                          
-n = l*10/mm                     # squares per lenght
-dpi = N * cm_i / l              # resolution
+# Constantes
+cm_i = 2.54   # cm por pulgada
+mm = 1     # tamaño de cada cuadrado
+cuadros_x = 50 * 10 // mm   # 250 cuadrados
+cuadros_y = 84 * 10 // mm   # 420 cuadrados
 
-x = np.linspace(0, N, N)
-y = np.linspace(0, N, N)
-X, Y = np.meshgrid(x, y)
-k = 2 * np.pi * n / N   
+pixeles_por_cuadro = 10     # resolución deseada → 20 px por 2 mm = 100 px/cm
 
-I0 = (((np.cos(k * X) > 0) ^ (np.cos(k * Y) > 0))).astype(float)
+# Tamaño total de la imagen
+Nx = cuadros_x * pixeles_por_cuadro  # 250*20 = 5000
+Ny = cuadros_y * pixeles_por_cuadro  # 420*20 = 8400
+
+# Crear patrón de tablero de ajedrez
+tile = np.array([[0, 1], [1, 0]], dtype=np.uint8)
+checkerboard = np.tile(tile, (cuadros_y//2, cuadros_x//2))
+
+# Escalamos cada cuadro a pixeles
+I0 = np.kron(checkerboard, np.ones((pixeles_por_cuadro, pixeles_por_cuadro)))
+
+# Convertimos a 16 bits
 I0_16bit = (I0 * 65535).astype(np.uint16)
 
+# Resolución en DPI
+dpi = pixeles_por_cuadro * (10 / mm) * cm_i  # = 100 px/cm * 2.54 = 254 dpi
+
+# Guardar imagen
 tifffile.imwrite(
-    'patron.tiff',
+    'patron_1_5mm.tiff',
     I0_16bit,
     photometric='minisblack',
     resolution=(dpi, dpi),
-    resolutionunit='inch'  
+    resolutionunit='inch'
 )
